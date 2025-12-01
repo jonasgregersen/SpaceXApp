dbb27f9e6f0ef9e157b3add731029abac6a2dbfd
jonasgregersen <eaa24jbg@students.eaaa.dk>
2025-12-01 08:48:35 +0100

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Jonas Gregersen on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
